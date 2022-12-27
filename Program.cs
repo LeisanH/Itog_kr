11ab4e92460e7ac7baa30e9fca93a931aa1f16ad
Leisan <kapatyn@mail.ru>
2022-12-28 02:57:02 +0500

    Решение задачи

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] array1 = new string[5] { "123", "-2", "1576", "computer", "science" };
+string[] array2 = new string[array1.Length];
+
+void SArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SArray(array1, array2);
+PrintArray(array2);
